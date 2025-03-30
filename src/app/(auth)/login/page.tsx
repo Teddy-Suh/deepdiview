5feb35bb0965f7781cb44f4645b43eeda7d27c71
Teddy-Suh <t3ddy.suh@gmail.com>
2025-03-30 16:12:05 +0900

    ✨ Feat: add ApiCient and integrate NextAuth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link'
+import LoginForm from './LoginForm'
+
 export default function LoginPage() {
-  return <div>로그인 페이지</div>
+  return (
+    <div>
+      <h2>로그인 페이지</h2>
+      <div>
+        <LoginForm />
+        <Link href='/register' className='btn btn-secondary'>
+          회원가입
+        </Link>
+      </div>
+    </div>
+  )
 }
