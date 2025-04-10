c427bbc6ce48ba993fc75c2f89f25758b373c3e7
Teddy-Suh <t3ddy.suh@gmail.com>
2025-04-10 15:48:30 +0900

    ✨ Feat: BoardCreatePage

--- src/app/board/create/page.tsx
+++ src/app/board/create/page.tsx
@@ -1,3 +1,13 @@
+export const dynamic = 'force-dynamic'
+
+import ReviewForm from '@/components/form/ReviewForm'
+import { createBoardReviewAction } from './actions'
+
 export default function BoardCreatePage() {
-  return <div>시청 인증된 사람만 접근할 수 있는 이주의 영화 리뷰 작성 페이지</div>
+  return (
+    <>
+      <h2>시청 인증된 사람만 접근할 수 있는 이주의 영화 리뷰 작성 페이지</h2>
+      <ReviewForm action={createBoardReviewAction} />
+    </>
+  )
 }
