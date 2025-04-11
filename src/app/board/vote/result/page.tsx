63d4a9a93454006e21cec567b6301e2576579b8c
Teddy-Suh <t3ddy.suh@gmail.com>
2025-04-11 13:24:20 +0900

    ✨ Feat: add VoteResultPage

--- src/app/board/vote/result/page.tsx
+++ src/app/board/vote/result/page.tsx
@@ -1,3 +1,41 @@
-export default function VoteResultPage() {
-  return <div>진행중인 투표 결과 페이지</div>
+export const dynamic = 'force-dynamic'
+
+import { getMovie } from '@/lib/api/movie'
+import { getVoteResult } from '@/lib/api/vote'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default async function VoteResultPage() {
+  const { results } = await getVoteResult()
+  const movieWithResultPromises = results.map(async (result) => {
+    const movie = await getMovie(result.tmdbId.toString())
+    return {
+      ...result,
+      ...movie,
+    }
+  })
+  const movies = await Promise.all(movieWithResultPromises)
+
+  return (
+    <>
+      <h2>진행중인 투표 결과</h2>
+      <ul className='flex'>
+        {movies.map((movie) => (
+          <li key={movie.tmdbId}>
+            <p>{movie.rank}위</p>
+            <p>{movie.title}</p>
+            <p>{movie.voteCount}표</p>
+            <Link href={`/movies/${movie.id}`}>
+              <Image
+                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                alt='영화 포스터'
+                width={100}
+                height={150}
+              />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  )
 }
