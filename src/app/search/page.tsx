c25e07ffe8024229d1b40ce148bdfdfc50cbe2d9
Teddy-Suh <t3ddy.suh@gmail.com>
2025-05-13 15:41:27 +0900

    ♻️ Refactor: update SearchPage with recent updates

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,68 +1,103 @@
-import SearchForm from '@/components/form/SearchForm'
+import SearchHeader from '@/components/layout/MobileHeader/SearchHeader'
 import { getSearchedMovies } from '@/lib/api/movie'
-import Image from 'next/image'
+import SearchMovieItem from './SearchMovieItem'
 import Link from 'next/link'
+import clsx from 'clsx'
 
 export default async function SearchPage({
   searchParams,
 }: {
-  searchParams: Promise<{ title?: string }>
+  searchParams: Promise<{ title?: string; page?: string }>
 }) {
-  const { title = '' } = await searchParams
+  const { title = '', page = '1' } = await searchParams
+  const trimmedTitle = title.trim()
+  const pageNumber = Number(page)
 
-  // 검색어가 없는 경우
-  if (!title.trim()) {
-    return (
-      <>
-        <div className='md:hidden'>
-          <SearchForm />
-        </div>
-        <p>검색어를 입력해주세요.</p>
-      </>
-    )
-  }
-
-  // 검색 결과가 없는 경우
-  let movies = []
-  try {
-    movies = (await getSearchedMovies({ title, page: 0 })).content
-  } catch (error) {
-    const errorCode = (error as Error).message
-    if (errorCode === 'KEYWORD_NOT_FOUND') {
-      return (
-        <>
-          <div className='md:hidden'>
-            <SearchForm />
-          </div>
-          <p>{title}에 대한 검색 결과가 없습니다.</p>
-        </>
-      )
-    } else {
-      throw error
-    }
-  }
+  const {
+    content: movies,
+    totalElements,
+    totalPages,
+  } = await getSearchedMovies({ title: trimmedTitle, page: pageNumber - 1 })
 
   return (
     <>
-      <div className='md:hidden'>
-        <SearchForm />
+      <SearchHeader />
+      <div className='container-wrapper'>
+        {!trimmedTitle ? (
+          <p className='py-4'>검색어를 입력해 주세요.</p>
+        ) : movies.length === 0 ? (
+          <p className='py-4'>
+            &quot;<span className='font-bold'>{trimmedTitle}</span>&quot;에 대한 검색 결과가
+            없습니다.
+          </p>
+        ) : (
+          <>
+            <p className='py-4'>
+              &quot;<span className='font-bold'>{trimmedTitle}</span>&quot;의 검색 결과 (
+              {totalElements})
+            </p>
+            <ul className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+              {movies.map((movie) => (
+                <li key={movie.id}>
+                  <SearchMovieItem movie={movie} />
+                  <hr className='mt-4 text-gray-700' />
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+        {/* 페이지네이션 */}
+        {totalPages > 1 && (
+          <div className='join mt-2 w-full justify-center md:mt-4'>
+            {pageNumber > 3 && (
+              <>
+                <Link
+                  className='join-item btn btn-sm md:btn-md'
+                  key={1}
+                  href={`/search?title=${encodeURIComponent(trimmedTitle)}&page=1`}
+                >
+                  1
+                </Link>
+                {pageNumber > 4 && (
+                  <button className='join-item btn btn-sm md:btn-md btn-disabled'>···</button>
+                )}
+              </>
+            )}
+            <>
+              {Array.from({ length: 5 }).map((_, i) => {
+                const targetPage = pageNumber - 2 + i
+                if (targetPage < 1 || targetPage > totalPages) return null
+                return (
+                  <Link
+                    className={clsx(
+                      'join-item btn btn-sm md:btn-md',
+                      targetPage === pageNumber && 'bg-primary pointer-events-none'
+                    )}
+                    key={targetPage}
+                    href={`/search?title=${encodeURIComponent(trimmedTitle)}&page=${targetPage}`}
+                  >
+                    {targetPage}
+                  </Link>
+                )
+              })}
+              {pageNumber + 2 < totalPages && (
+                <>
+                  {pageNumber + 3 < totalPages && (
+                    <button className='join-item btn btn-sm btn-disabled md:btn-md'>···</button>
+                  )}
+                  <Link
+                    className='join-item btn btn-sm md:btn-md'
+                    key={totalPages}
+                    href={`/search?title=${encodeURIComponent(trimmedTitle)}&page=${totalPages}`}
+                  >
+                    {totalPages}
+                  </Link>
+                </>
+              )}
+            </>
+          </div>
+        )}
       </div>
-      <h2>{title}에 대한 검색 결과</h2>
-      <ul>
-        {movies.map((movie) => (
-          <li key={movie.id}>
-            <Link href={`/movies/${movie.id}`}>
-              <p>{movie.title}</p>
-              <Image
-                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                alt='영화 포스터'
-                width={100}
-                height={150}
-              />
-            </Link>
-          </li>
-        ))}
-      </ul>
     </>
   )
 }
