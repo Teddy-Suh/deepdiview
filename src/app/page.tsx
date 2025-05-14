616ecd5acc5d262fd2b22833a43de935dd9b027c
Teddy-Suh <t3ddy.suh@gmail.com>
2025-05-14 17:20:03 +0900

    ♻️ Refactor: extract LatestReviewSection from HomePage and adjust layout spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,12 @@
 export const dynamic = 'force-dynamic'
 
 import BaseHeader from '@/components/layout/MobileHeader/BaseHeader'
+import LatestReviewSection from '@/components/ui/LatestReviewSection'
 import MovieCarousel from '@/components/ui/MovieCarousel'
 import OverlaidMovieHero from '@/components/ui/OverlaidMovieHero'
-import ReviewCarousel from '@/components/ui/ReviewCarousel'
 import { getIsSunday, getThisWeekMovieId } from '@/lib/api/discussion'
 import { getMovie, getPopularMovies } from '@/lib/api/movie'
 import { getLatestReviews } from '@/lib/api/review'
-import { ChevronRight } from 'lucide-react'
-import Link from 'next/link'
 
 export default async function HomePage() {
   const [popularMovies, { isSunday }, latestReviews, { tmdbId }] = await Promise.all([
@@ -23,24 +21,15 @@ export default async function HomePage() {
   return (
     <>
       <BaseHeader />
-      <div className='space-y-8'>
-        <OverlaidMovieHero movie={thisWeekMovie} isSunday={isSunday} />
+      <OverlaidMovieHero movie={thisWeekMovie} isSunday={isSunday} />
 
+      <div className='space-y-8'>
         <section className='container-wrapper'>
           <h3 className='mb-3 text-xl font-semibold'>인기 영화</h3>
           <MovieCarousel movies={popularMovies} />
         </section>
 
-        <section className='container-wrapper'>
-          <div className='flex justify-between'>
-            <h3 className='mb-3 text-xl font-semibold'>최신 리뷰</h3>
-            <Link className='flex' href={'/reviews'}>
-              더보기
-              <ChevronRight />
-            </Link>
-          </div>
-          <ReviewCarousel reviews={latestReviews.content} />
-        </section>
+        <LatestReviewSection latestReviews={latestReviews.content} href='/reviews' />
       </div>
     </>
   )
