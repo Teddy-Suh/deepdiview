f6f2cc31e687b39332269d5f9e799bfd68d34f23
Teddy-Suh <t3ddy.suh@gmail.com>
2025-05-30 09:55:53 +0900

    ✨ Feat: add profile link to ReviewItem and update LikeButton

--- src/components/ui/LikeButton.tsx
+++ src/components/ui/LikeButton.tsx
@@ -9,10 +9,12 @@ export default function LikeButton({
   likedByUser,
   likeCount,
   reviewId,
+  readOnly = false,
 }: {
   likedByUser: boolean | null
   likeCount: number
   reviewId: string
+  readOnly: boolean
 }) {
   const [state, formAction] = useActionState(toggleLikeAction.bind(null, reviewId), {
     likedByUser,
@@ -20,13 +22,37 @@ export default function LikeButton({
     message: '',
   })
 
+  if (readOnly || likedByUser === null)
+    return (
+      <div className='flex gap-1'>
+        <Heart className={clsx(likedByUser && 'fill-primary stroke-primary')} />
+        <p>{likeCount}</p>
+      </div>
+    )
+
   return (
-    <form action={formAction}>
-      <button className='flex gap-1' disabled={likedByUser === null}>
-        <Heart className={clsx(state.likedByUser && 'fill-primary stroke-primary')} />
-        <p>{state.likeCount}</p>
-      </button>
+    <>
+      <form action={formAction}>
+        <div className='flex gap-1'>
+          <button
+            className={clsx(
+              'cursor-pointer active:animate-ping',
+              !state.likedByUser && 'animate-pulse'
+            )}
+            type='submit'
+          >
+            <Heart
+              className={clsx(
+                state.likedByUser
+                  ? 'fill-primary stroke-primary'
+                  : 'hover:fill-primary hover:stroke-primary'
+              )}
+            />
+          </button>
+          <p>{state.likeCount}</p>
+        </div>
+      </form>
       {state.message && <p>좋아요 실패</p>}
-    </form>
+    </>
   )
 }
