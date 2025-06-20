5b318aba9f1956d8620af91aeaaa4f5edb23c592
Teddy-Suh <t3ddy.suh@gmail.com>
2025-06-20 14:12:18 +0900

    ✨ Feat: add Error and NotFound

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,13 @@
-'use client'
-
 import BaseHeader from '@/components/layout/MobileHeader/BaseHeader'
-import { CircleAlert } from 'lucide-react'
 import Link from 'next/link'
 
-export default function Error() {
+export default function NotFound() {
   return (
     <>
       <BaseHeader />
       <div className='container-wrapper flex flex-1 flex-col items-center justify-center gap-6'>
-        <CircleAlert size={100} />
-        <p className='text-lg font-semibold'>예상치 못한 에러가 발생했습니다.</p>
+        <p className='text-8xl font-black'>404</p>
+        <p className='text-lg font-semibold'>페이지를 찾을 수 없습니다.</p>
         <Link className='btn btn-primary rounded-2xl' href='/'>
           홈으로 이동
         </Link>
