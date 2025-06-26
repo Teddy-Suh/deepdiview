0442e8808c380acf3c9e70adab00ec16e249e36e
Teddy-Suh <t3ddy.suh@gmail.com>
2025-06-26 19:30:22 +0900

    🔧 Chore: fix brand name inconsistency

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Footer() {
   return (
     <footer className='dark:bg-base-300 hidden border-t-1 border-t-gray-300 bg-black/5 p-4 text-center text-sm text-gray-500 md:block dark:border-t-gray-800'>
-      <p>© 2025 Deepdiview. All Rights Reserved.</p>
+      <p>© 2025 DeepDiview. All Rights Reserved.</p>
       <div className='mt-2 space-x-4'>
         <Link
           href='https://github.com/Teddy-Suh'
